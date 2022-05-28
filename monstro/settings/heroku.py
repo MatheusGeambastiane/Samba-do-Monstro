8d57534a83569641d1833e6765920abf23458216
mgeambastiane <mgeambastiane@gmail.com>
2022-05-28 14:21:43 -0300

    Subindo arquivos pra heroku

---
+++ monstro/settings/heroku.py
@@ -0,0 +1,15 @@
+import environ
+
+from monstro.settings.base import *
+
+env = environ.env()
+
+DEBUG = env.bool("DEBUG", False)
+
+SECRET_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+DATABASES = {
+    "default": env.db(),
+}
