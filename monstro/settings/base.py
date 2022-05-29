5ed99b44b742532799ddeb3987d124c957ab1051
mgeambastiane <mgeambastiane@gmail.com>
2022-05-29 01:43:56 -0300

    subindo heroku on django

--- monstro/settings/base.py
+++ monstro/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+#3rd Party
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -139,3 +141,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
